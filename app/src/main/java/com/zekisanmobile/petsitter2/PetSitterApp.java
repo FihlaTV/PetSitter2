e77cfe57b6519b6b0d38c08b6c0620239c3f0038
Ezequiel <zeki-san@hotmail.com>
2016-04-29 23:16:21 -0300

    Configs.

--- app/src/main/java/com/zekisanmobile/petsitter2/PetSitterApp.java
+++ app/src/main/java/com/zekisanmobile/petsitter2/PetSitterApp.java
@@ -2,6 +2,8 @@ package com.zekisanmobile.petsitter2;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
+
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
@@ -16,5 +18,11 @@ public class PetSitterApp extends Application {
                 .deleteRealmIfMigrationNeeded()
                 .build();
         Realm.setDefaultConfiguration(config);
+
+        Stetho.InitializerBuilder initializerBuilder = Stetho.newInitializerBuilder(this);
+        initializerBuilder.enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this));
+        initializerBuilder.enableDumpapp(Stetho.defaultDumperPluginsProvider(this));
+        Stetho.Initializer initializer = initializerBuilder.build();
+        Stetho.initialize(initializer);
     }
 }
