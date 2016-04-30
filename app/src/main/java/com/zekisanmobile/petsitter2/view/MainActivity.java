f97783f811e1db68c1b84e31f30a569745b470b4
Ezequiel <zeki-san@hotmail.com>
2016-04-29 21:07:06 -0300

    Added apt and butterknife to gradle.

--- app/src/main/java/com/zekisanmobile/petsitter2/view/MainActivity.java
+++ app/src/main/java/com/zekisanmobile/petsitter2/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.zekisanmobile.petsitter2;
+package com.zekisanmobile.petsitter2.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.zekisanmobile.petsitter2.R;
 
 public class MainActivity extends AppCompatActivity {
 
