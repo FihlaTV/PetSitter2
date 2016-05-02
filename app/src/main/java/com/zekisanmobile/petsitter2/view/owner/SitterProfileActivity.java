f09717ec83f59045d536616d8248e06728f8a8d0
Ezequiel <zeki-san@hotmail.com>
2016-05-01 22:01:10 -0300

    Added info to sitter profile.

--- app/src/main/java/com/zekisanmobile/petsitter2/view/owner/SitterProfileActivity.java
+++ app/src/main/java/com/zekisanmobile/petsitter2/view/owner/SitterProfileActivity.java
@@ -3,7 +3,10 @@ package com.zekisanmobile.petsitter2.view.owner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 import com.zekisanmobile.petsitter2.R;
@@ -12,6 +15,10 @@ import com.zekisanmobile.petsitter2.model.UserModel;
 import com.zekisanmobile.petsitter2.session.SessionManager;
 import com.zekisanmobile.petsitter2.vo.Sitter;
 import com.zekisanmobile.petsitter2.vo.User;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,6 +39,18 @@ public class SitterProfileActivity extends AppCompatActivity {
     @BindView(R.id.iv_sitter)
     ImageView ivSitter;
 
+    @BindView(R.id.tv_value_hour)
+    TextView tvValueHour;
+
+    @BindView(R.id.tv_district)
+    TextView tvDistrict;
+
+    @BindView(R.id.tv_about_me)
+    TextView tvAboutMe;
+
+    @BindView(R.id.lv_pets)
+    ListView lvPets;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,12 +62,28 @@ public class SitterProfileActivity extends AppCompatActivity {
 
         defineMembers();
         configureToolbar();
+        defineViewsValues();
     }
 
     @Override
     protected void onDestroy() {
         realm.close();
         super.onDestroy();
+    }
+
+    private void defineViewsValues() {
+        NumberFormat format = NumberFormat.getCurrencyInstance();
+        tvValueHour.setText(format.format(sitter.getValueHour()) + "/h");
+        tvDistrict.setText(sitter.getDistrict());
+        tvAboutMe.setText(sitter.getAboutMe());
+
+        List<String> animalsForList = new ArrayList<>();
+        for (int i = 0; i < sitter.getAnimals().size(); i++) {
+            animalsForList.add(sitter.getAnimals().get(i).getName());
+        }
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,
+                animalsForList);
+        lvPets.setAdapter(adapter);
     }
 
     private void defineMembers() {
