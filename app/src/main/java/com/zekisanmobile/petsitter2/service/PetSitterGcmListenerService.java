e52a692c1479c2fed92e4c21b4557933ae21bcac
Ezequiel <ezequiel.oliveira@iwaysoft.com.br>
2016-05-04 17:24:12 -0300

    Working on GCM.

--- app/src/main/java/com/zekisanmobile/petsitter2/service/PetSitterGcmListenerService.java
+++ app/src/main/java/com/zekisanmobile/petsitter2/service/PetSitterGcmListenerService.java
@@ -1,6 +1,47 @@
 package com.zekisanmobile.petsitter2.service;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
 import com.google.android.gms.gcm.GcmListenerService;
+import com.zekisanmobile.petsitter2.R;
+import com.zekisanmobile.petsitter2.view.login.LoginActivity;
 
 public class PetSitterGcmListenerService extends GcmListenerService {
+
+    @Override
+    public void onMessageReceived(String from, Bundle data) {
+        super.onMessageReceived(from, data);
+        Log.d("GCM Received on device.", data.toString());
+        sendNotification("oi");
+    }
+
+    private void sendNotification(String message) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.paw)
+                .setContentTitle("Nova msg recebida")
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+
 }
