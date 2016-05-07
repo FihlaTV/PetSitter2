1f21a490141de9d8dbbcb192ba63b627d525e4ed
Ezequiel <zeki-san@hotmail.com>
2016-05-07 16:01:05 -0300

    Configured Google Maps on job details.

--- app/src/main/java/com/zekisanmobile/petsitter2/view/sitter/JobDetailsActivity.java
+++ app/src/main/java/com/zekisanmobile/petsitter2/view/sitter/JobDetailsActivity.java
@@ -4,6 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.zekisanmobile.petsitter2.R;
 import com.zekisanmobile.petsitter2.model.JobModel;
 import com.zekisanmobile.petsitter2.util.Config;
@@ -13,7 +20,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.realm.Realm;
 
-public class JobDetailsActivity extends AppCompatActivity {
+public class JobDetailsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private Realm realm;
 
@@ -35,6 +42,7 @@ public class JobDetailsActivity extends AppCompatActivity {
 
         defineMembers();
         configureToolbar();
+        configureMap();
     }
 
     @Override
@@ -55,5 +63,20 @@ public class JobDetailsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    private void configureMap() {
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng latLng = new LatLng(job.getOwner().getLatitude(),
+                job.getOwner().getLongitude());
+        googleMap.addMarker(new MarkerOptions().position(latLng));
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 16);
+        googleMap.animateCamera(cameraUpdate);
     }
 }
