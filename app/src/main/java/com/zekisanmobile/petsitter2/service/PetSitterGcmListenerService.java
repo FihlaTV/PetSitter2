868cc2fc8fd664238a8a8a3d25a740d0dc24dc20
Ezequiel <zeki-san@hotmail.com>
2016-05-15 19:56:33 -0300

    Finished notifications.

--- app/src/main/java/com/zekisanmobile/petsitter2/service/PetSitterGcmListenerService.java
+++ app/src/main/java/com/zekisanmobile/petsitter2/service/PetSitterGcmListenerService.java
@@ -10,9 +10,18 @@ import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.android.gms.gcm.GcmListenerService;
 import com.zekisanmobile.petsitter2.R;
-import com.zekisanmobile.petsitter2.view.login.LoginActivity;
+import com.zekisanmobile.petsitter2.model.JobModel;
+import com.zekisanmobile.petsitter2.util.Config;
+import com.zekisanmobile.petsitter2.util.JobsStatusString;
+import com.zekisanmobile.petsitter2.view.sitter.SitterJobDetailsActivity;
+import com.zekisanmobile.petsitter2.vo.Job;
+
+import java.io.IOException;
+
+import io.realm.Realm;
 
 public class PetSitterGcmListenerService extends GcmListenerService {
 
@@ -20,11 +29,23 @@ public class PetSitterGcmListenerService extends GcmListenerService {
     public void onMessageReceived(String from, Bundle data) {
         super.onMessageReceived(from, data);
         Log.d("GCM Received on device.", data.toString());
-        sendNotification("oi");
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            Job job = mapper.readValue(data.getString("body"), Job.class);
+            Realm realm = Realm.getDefaultInstance();
+            JobModel jobModel = new JobModel(realm);
+            jobModel.save(job);
+            realm.close();
+            sendNotification(job);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void sendNotification(String message) {
-        Intent intent = new Intent(this, LoginActivity.class);
+    private void sendNotification(Job job) {
+        Intent intent = new Intent(this, SitterJobDetailsActivity.class);
+        intent.putExtra(Config.JOB_ID, job.getId());
+        intent.putExtra(Config.JOB_STATUS, JobsStatusString.NEW);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
@@ -32,8 +53,8 @@ public class PetSitterGcmListenerService extends GcmListenerService {
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.paw)
-                .setContentTitle("Nova msg recebida")
-                .setContentText(message)
+                .setContentTitle("Nova solicitação de trabalho")
+                .setContentText("Toque para visualizar")
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent);
