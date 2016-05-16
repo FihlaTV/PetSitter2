5067d6b167a45280e47e7268bc2d736e20cd6b68
Ezequiel <zeki-san@hotmail.com>
2016-05-16 20:41:52 -0300

    Created reply rate activity.

---
+++ app/src/main/java/com/zekisanmobile/petsitter2/view/sitter/ReplyRateActivity.java
@@ -0,0 +1,15 @@
+package com.zekisanmobile.petsitter2.view.sitter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zekisanmobile.petsitter2.R;
+
+public class ReplyRateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reply_rate);
+    }
+}
