1c6147cdff03bdb5b24f3df127a787fe7adf9eaf
Ezequiel <zeki-san@hotmail.com>
2016-05-16 20:52:41 -0300

    Configured toolbar.

--- app/src/main/java/com/zekisanmobile/petsitter2/view/sitter/ReplyRateActivity.java
+++ app/src/main/java/com/zekisanmobile/petsitter2/view/sitter/ReplyRateActivity.java
@@ -2,14 +2,32 @@ package com.zekisanmobile.petsitter2.view.sitter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.zekisanmobile.petsitter2.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ReplyRateActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reply_rate);
+
+        ButterKnife.bind(this);
+
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        toolbar.setTitle(getString(R.string.my_rate));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 }
