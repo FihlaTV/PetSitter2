6455fdb47db37dea9acd949f59399d0b8c7aa4ba
Ezequiel <zeki-san@hotmail.com>
2016-05-21 14:58:27 -0300

    Creating register activity.

--- app/src/main/java/com/zekisanmobile/petsitter2/view/register/RegisterHomeActivity.java
+++ app/src/main/java/com/zekisanmobile/petsitter2/view/register/RegisterHomeActivity.java
@@ -1,15 +1,45 @@
 package com.zekisanmobile.petsitter2.view.register;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.zekisanmobile.petsitter2.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class RegisterHomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_home);
+
+        ButterKnife.bind(this);
+
+        configureToolbar();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void configureToolbar() {
+        toolbar.setTitle(getString(R.string.welcome));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 }
