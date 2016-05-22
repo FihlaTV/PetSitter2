308904b255a0114aebb309ed50e2e31aa186ce8c
Ezequiel <zeki-san@hotmail.com>
2016-05-21 21:13:17 -0300

    Finished layout for location register.

--- app/src/main/java/com/zekisanmobile/petsitter2/view/register/AddressRegisterActivity.java
+++ app/src/main/java/com/zekisanmobile/petsitter2/view/register/AddressRegisterActivity.java
@@ -1,7 +1,7 @@
 package com.zekisanmobile.petsitter2.view.register;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.zekisanmobile.petsitter2.R;
