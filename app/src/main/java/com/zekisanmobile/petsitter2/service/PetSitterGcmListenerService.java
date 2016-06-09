bd10e167a3dafd5f4b4cb8be91dab92201303df3
Ezequiel <zeki-san@hotmail.com>
2016-06-09 20:02:26 -0300

    Created notifications for accepted and rejected jobs.

--- app/src/main/java/com/zekisanmobile/petsitter2/service/PetSitterGcmListenerService.java
+++ app/src/main/java/com/zekisanmobile/petsitter2/service/PetSitterGcmListenerService.java
@@ -16,6 +16,7 @@ import com.zekisanmobile.petsitter2.R;
 import com.zekisanmobile.petsitter2.model.JobModel;
 import com.zekisanmobile.petsitter2.util.Config;
 import com.zekisanmobile.petsitter2.util.JobsStatusString;
+import com.zekisanmobile.petsitter2.view.owner.OwnerJobDetailsActivity;
 import com.zekisanmobile.petsitter2.view.sitter.SitterJobDetailsActivity;
 import com.zekisanmobile.petsitter2.vo.Job;
 
@@ -29,16 +30,22 @@ public class PetSitterGcmListenerService extends GcmListenerService {
     public void onMessageReceived(String from, Bundle data) {
         super.onMessageReceived(from, data);
         Log.d("GCM Received on device.", data.toString());
+        Job job = getJobFromMessage(data);
         String type = data.getString("type");
         switch (type) {
             case "job":
-                getJobFromMessage(data);
+                sendNewJobNotification(job);
                 break;
-
+            case "job_rejected":
+                sendJobRejectedNotification(job);
+                break;
+            case "job_accepted":
+                sendAcceptedJobNotification(job);
+                break;
         }
     }
 
-    private void getJobFromMessage(Bundle data) {
+    private Job getJobFromMessage(Bundle data) {
         ObjectMapper mapper = new ObjectMapper();
         try {
             Job job = mapper.readValue(data.getString("body"), Job.class);
@@ -46,13 +53,15 @@ public class PetSitterGcmListenerService extends GcmListenerService {
             JobModel jobModel = new JobModel(realm);
             jobModel.save(job);
             realm.close();
-            sendNotification(job);
+
+            return job;
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return null;
     }
 
-    private void sendNotification(Job job) {
+    private void sendNewJobNotification(Job job) {
         Intent intent = new Intent(this, SitterJobDetailsActivity.class);
         intent.putExtra(Config.JOB_ID, job.getId());
         intent.putExtra(Config.JOB_STATUS, JobsStatusString.NEW);
@@ -60,10 +69,36 @@ public class PetSitterGcmListenerService extends GcmListenerService {
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
+        sendNotification(pendingIntent, "Nova solicitação de trabalho");
+    }
+
+    private void sendAcceptedJobNotification(Job job) {
+        Intent intent = new Intent(this, OwnerJobDetailsActivity.class);
+        intent.putExtra(Config.JOB_ID, job.getId());
+        intent.putExtra(Config.JOB_STATUS, JobsStatusString.NEW);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        sendNotification(pendingIntent, "Sua reserva foi confirmada.");
+    }
+
+    private void sendJobRejectedNotification(Job job) {
+        Intent intent = new Intent(this, OwnerJobDetailsActivity.class);
+        intent.putExtra(Config.JOB_ID, job.getId());
+        intent.putExtra(Config.JOB_STATUS, JobsStatusString.NEW);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        sendNotification(pendingIntent, "Sua reserva foi rejeitada.");
+    }
+
+    private void sendNotification(PendingIntent pendingIntent, String title) {
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.paw)
-                .setContentTitle("Nova solicitação de trabalho")
+                .setContentTitle(title)
                 .setContentText("Toque para visualizar")
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
@@ -74,5 +109,4 @@ public class PetSitterGcmListenerService extends GcmListenerService {
 
         notificationManager.notify(0, notificationBuilder.build());
     }
-
 }
